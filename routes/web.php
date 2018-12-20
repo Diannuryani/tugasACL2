5d691f0fa71aef2f2c0e8874b1077c03fdc3a251
Diannuryani <diannuryani05@gmail.com>
2018-12-20 20:41:12 +0700

    tugas fix

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::GET('admin/home','adminController@index');
+Route::GET('admin','admin\LoginController@showLoginFrom')->name('admin.login');
+Route::POST('admin','admin\LoginController@login');
+Route::POST('admin-password/email','admin\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+Route::GET('admin-password/reset','admin\ForgotPasswordController@sendResetLinkRequestFrom')->name('admin.password.request');
+Route::POST('admin-password/reset','admin\ResetPasswordController@reset');
+Route::GET('admin-password/reset/{token}','admin\ResetPasswordController@showResetFrom')->name('admin.password.reset');
